b0cf028949d86d3a8ea74bdcf7c3017a5012eb10
kirengamartial <kirengamartial@gmail.com>
2024-10-03 22:24:25 +0200

    multi containers

--- backend/app.js
+++ backend/app.js
@@ -83,8 +83,10 @@ app.delete('/goals/:id', async (req, res) => {
   }
 });
 
+// mongodb://max:secret@mongodb:27017/course-goals?authSource=admin
+
 mongoose.connect(
-  'mongodb://mongodb:27017/course-goals',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_SECRET}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
